8acfed74b375b83f776ce7d9122ec8108c03b61f
LaurenGifford <lauren.gifford.17@gmail.com>
2021-02-03 12:06:20 -0500

    complete

--- index.js
+++ index.js
@@ -59,7 +59,11 @@ animalToRemove.remove()
 /************************** EVENTS JS MINI CHALLENGE ******************************/
 
 /***** Deliverable 1 *****/
-function toggleColor(element) {
+header.addEventListener('click', toggleColor)
+
+function toggleColor(event) {
+    const element = event.target
+
     if (element.style.color === "green") {
         element.style.color = "black"
     } else {
@@ -67,6 +71,70 @@ function toggleColor(element) {
     }
 }
 
+
 /***** Deliverable 2 *****/
 
+const likeButton = document.querySelector('.like-button')
+
+likeButton.addEventListener('click', increaseLikes)
+
+// Pesimistic render
+function increaseLikes() {
+    const moreLikes = traveler.likes++
+    likes.textContent = `${moreLikes} Likes`
+}
+
+// Optimistic render
+// function increaseLikes(e) {
+//     let likeP = e.target.parentElement.querySelector('.likes')
+//     let likeText = likeP.innerText
+//     let likeCount = parseInt(likeText.split(" ")[0])
+    
+//     let increasedLikes = likeCount+1
+//     likeP.textContent = `${increasedLikes} Likes`
+// }
+
 /***** Deliverable 3 *****/
+
+const form = document.querySelector('#new-animal-sighting-form')
+// form.addEventListener('submit', function (event){
+//     event.preventDefault()
+//     console.log(event)
+
+//     const inputOne = event.target[0].value
+//     const inputTwo = event.target[1].value
+//     const inputThree = event.target[2].value 
+//     const inputFour = event.target[3].value
+
+//     const animalObject = {
+//         species: inputOne,
+//         link: inputTwo,
+//         photo: inputThree,
+//         description: inputFour,
+//     }
+
+//     renderAnimalSightingPost(animalObject)
+//     event.target.reset()
+// })
+
+// OR
+form.addEventListener('submit', formData)
+
+function formData(e){
+    e.preventDefault()
+
+    const species = e.target.species.value
+    const link = e.target.link.value
+    const photo = e.target.photo.value
+    const description = e.target.description.value
+
+    const animalObject = {
+        species: species,
+        link: link,
+        photo: photo,
+        description: description
+    }
+
+    renderAnimalSightingPost(animalObject)
+    e.target.reset()
+}
